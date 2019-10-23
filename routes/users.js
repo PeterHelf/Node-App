954e366eb757fa20f5ce8169bf17a41565054255
PeterHelf <106546@fhwn.ac.at>
2019-10-24 00:16:46 +0200

    release

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send([]);
+  res.send(['user1','user2']);
 });
 
 module.exports = router;
