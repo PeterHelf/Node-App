f3e36326959089e58f7466b3721ff32d15878ca4
PeterHelf <106546@fhwn.ac.at>
2019-10-24 01:17:23 +0200

    changed user array

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send([]);
+  res.send(['user1','user2','user3']);
 });
 
 module.exports = router;
